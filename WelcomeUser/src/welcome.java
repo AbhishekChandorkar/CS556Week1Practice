ced24c1aaab5679daa4b56ad0e0ffd8c78cf2365
tejaswini <tejaswiniav@gmail.com>
2016-04-01 11:33:31 -0700

    Initial checkin

---
+++ WelcomeUser/src/welcome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class welcome {
+	public static void main(String[] args) {
+		System.out.println("Hi Welcome to class CS 556");
+		System.out.println("Please enter your name: ");
+		Scanner S = new Scanner(System.in);
+		String name = S.nextLine();
+		System.out.println("Please enter your email: ");
+		String email = S.nextLine();
+	}
+}
